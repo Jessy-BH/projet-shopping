f2ce92124dcea38b76ce1ddd325d4dece3b718c9
Jessy-BH <berthaultjessy@gmail.com>
2023-02-14 17:05:21 +0100

    api + styled compo

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -1,9 +1,26 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 const Home = () => {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    fetch('https://fakestoreapi.com/products')
+      .then(response => response.json())
+      .then(data => setProducts(data));
+  }, []);
+  
   return (
-    <div>Home</div>
-  )
+    <div>
+      {products.map(product => (
+        <div key={product.id}>
+          <h2>{product.title}</h2>
+          <p>{product.description}</p>
+          <p>Price: ${product.price}</p>
+          <img src={product.image} alt={product.title} />
+        </div>
+      ))}
+    </div>
+  );
 }
 
 export default Home
